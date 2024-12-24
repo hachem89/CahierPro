6e71beead23367f5c41ffe0c9fdc6e59c3009242
Meddeb Hachem <68928285+hachem89@users.noreply.github.com>
2024-12-24 01:12:19 +0100

    change the fetching url

--- client/src/services/emailService.js
+++ client/src/services/emailService.js
@@ -1,7 +1,7 @@
 // File: /src/services/emailService.js
 const sendEmail = async (email, pdfData, filename) => {
   try {
-    const response = await fetch("http://localhost:5000/send-pdf", {
+    const response = await fetch("https://cahierpro-server.onrender.com/send-pdf", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, pdfData, filename }),
